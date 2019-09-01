ba7e798d373e4fc8ac0b4d530453c6c4ae4efcc3
n.kuklina <n.kuklina@corp.mail.ru>
2019-09-01 20:42:30 +0300

    medtner

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-window.onload = function () {
-    $('a.nav-link').click(function () {
-        $($(this).attr('data-href'))[0].scrollIntoView();
-        window.scrollBy(0,-50);
-    })
-};
